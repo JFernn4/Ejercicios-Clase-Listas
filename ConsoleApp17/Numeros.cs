55a8e02b70a5c45a9fca94ba5705d83f769dfaad
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-02 09:57:06 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp17/Numeros.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Numeros
+    {
+        public int Numero { get; set; }
+
+        public Numeros(int numero)
+        {
+            Numero = numero;
+        }
+        public static void IngresarNumeros(List<Numeros> listaNumeros)
+        {
+            int numero = 0;
+            Console.WriteLine("Ingrese un numero.");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Numeros numero1= new Numeros(numero);
+            listaNumeros.Add(numero1);
+        }
+        public static void MostrarNumeros(List<Numeros> listaNumeros)
+        {
+            foreach (Numeros numero in listaNumeros)
+            {
+                Console.WriteLine(numero.Numero);
+            }
+        }
+
+        public static void MultiplicarNumeros(List<Numeros> listaNumeros)
+        {
+            int multiplicacion = 1;
+            foreach (Numeros numero in listaNumeros)
+            {
+                multiplicacion *= numero.Numero;
+                Console.WriteLine(multiplicacion);
+            }
+        }
+    }
+}
---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleApp17;
+
+List<Numeros> listaNumeros= new List<Numeros>();
+bool menu = true;
+int opcion;
+while (menu)
+{
+    Console.Clear();
+    Console.WriteLine("1. Ingresar numeros");
+    Console.WriteLine("2. Mostrar numeros");
+    opcion= Convert.ToInt32(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            {
+                Console.Clear() ;
+                Numeros.IngresarNumeros(listaNumeros);
+                break;
+            }
+        case 2:
+            {
+                Console.Clear() ;
+                Numeros.MostrarNumeros(listaNumeros);
+                Console.ReadKey();
+                break;
+            }
+        case 3:
+            {
+                Console.Clear();
+                Numeros.MultiplicarNumeros(listaNumeros);
+                Console.ReadKey();
+                break;
+            }
+    }
+}
