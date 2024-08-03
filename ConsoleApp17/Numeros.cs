cbe5874f38223fb2802e959f12f6feacdd104f0f
Simon <124474593+xLGerardox@users.noreply.github.com>
2024-08-02 19:39:04 -0600

    Update Numeros.cs

--- ConsoleApp17/Numeros.cs
+++ ConsoleApp17/Numeros.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,19 +14,55 @@ namespace ConsoleApp17
         {
             Numero = numero;
         }
+
         public static void IngresarNumeros(List<Numeros> listaNumeros)
         {
-            int numero = 0;
-            Console.WriteLine("Ingrese un numero.");
-            numero = Convert.ToInt32(Console.ReadLine());
-            Numeros numero1= new Numeros(numero);
-            listaNumeros.Add(numero1);
+            try
+            {
+                Console.WriteLine("Ingrese un número:");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                Numeros numero1 = new Numeros(numero);
+                listaNumeros.Add(numero1);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Por favor, ingrese un número entero.");
+            }
         }
+
         public static void MostrarNumeros(List<Numeros> listaNumeros)
         {
+            Console.WriteLine("Lista de números:");
             foreach (Numeros numero in listaNumeros)
             {
                 Console.WriteLine(numero.Numero);
+            }
+        }
+
+        public static void SumarNumeros(List<Numeros> listaNumeros)
+        {
+            int suma = 0;
+            foreach (Numeros numero in listaNumeros)
+            {
+                suma += numero.Numero;
+            }
+            Console.WriteLine($"La suma de los números es: {suma}");
+        }
+
+        public static void RestarNumeros(List<Numeros> listaNumeros)
+        {
+            if (listaNumeros.Count > 0)
+            {
+                int resta = listaNumeros[0].Numero;
+                for (int i = 1; i < listaNumeros.Count; i++)
+                {
+                    resta -= listaNumeros[i].Numero;
+                }
+                Console.WriteLine($"La resta de los números es: {resta}");
+            }
+            else
+            {
+                Console.WriteLine("No hay números en la lista para restar.");
             }
         }
 
@@ -36,8 +72,41 @@ namespace ConsoleApp17
             foreach (Numeros numero in listaNumeros)
             {
                 multiplicacion *= numero.Numero;
-                Console.WriteLine(multiplicacion);
             }
+            Console.WriteLine($"La multiplicación de los números es: {multiplicacion}");
+        }
+
+        public static void DividirNumeros(List<Numeros> listaNumeros)
+        {
+            if (listaNumeros.Count > 0)
+            {
+                float division = listaNumeros[0].Numero;
+                for (int i = 1; i < listaNumeros.Count; i++)
+                {
+                    if (listaNumeros[i].Numero != 0)
+                    {
+                        division /= listaNumeros[i].Numero;
+                    }
+                    else
+                    {
+                        Console.WriteLine("resultado indefinido.");
+                        return;
+                    }
+                }
+                Console.WriteLine($"La división de los números es: {division}");
+            }
+            else
+            {
+                Console.WriteLine("No hay números en la lista para dividir.");
+            }
+        }
+
+        public static void MostrarOperaciones(List<Numeros> listaNumeros)
+        {
+            SumarNumeros(listaNumeros);
+            RestarNumeros(listaNumeros);
+            MultiplicarNumeros(listaNumeros);
+            DividirNumeros(listaNumeros);
         }
     }
 }
