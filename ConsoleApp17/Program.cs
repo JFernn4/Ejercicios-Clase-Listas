f8aa30c338eba8a4886cd81ecdb1e5101f7b0609
Simon <124474593+xLGerardox@users.noreply.github.com>
2024-08-02 19:37:03 -0600

    Update Program.cs
    se creo el menú desde 0

--- ConsoleApp17/Program.cs
+++ ConsoleApp17/Program.cs
@@ -1,35 +1,71 @@
-﻿using ConsoleApp17;
+using ConsoleApp17;
 
-List<Numeros> listaNumeros= new List<Numeros>();
-bool menu = true;
-int opcion;
-while (menu)
+class Program
 {
-    Console.Clear();
-    Console.WriteLine("1. Ingresar numeros");
-    Console.WriteLine("2. Mostrar numeros");
-    opcion= Convert.ToInt32(Console.ReadLine());
-    switch (opcion)
+    static void Main(string[] args)
     {
-        case 1:
+        List<Numeros> listaNumeros = new List<Numeros>();
+        bool menu = true;
+        int opcion;
+
+        while (menu)
+        {
+            Console.Clear();
+            Console.WriteLine("1. Ingresar números");
+            Console.WriteLine("2. Mostrar números");
+            Console.WriteLine("3. Sumar números");
+            Console.WriteLine("4. Restar números");
+            Console.WriteLine("5. Multiplicar números");
+            Console.WriteLine("6. Dividir números");
+            Console.WriteLine("7. Mostrar todas las operaciones");
+            Console.WriteLine("8. Salir");
+            Console.Write("Seleccione una opción: ");
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
             {
-                Console.Clear() ;
-                Numeros.IngresarNumeros(listaNumeros);
-                break;
+                case 1:
+                    Console.Clear();
+                    Numeros.IngresarNumeros(listaNumeros);
+                    break;
+                case 2:
+                    Console.Clear();
+                    Numeros.MostrarNumeros(listaNumeros);
+                    Console.ReadKey();
+                    break;
+                case 3:
+                    Console.Clear();
+                    Numeros.SumarNumeros(listaNumeros);
+                    Console.ReadKey();
+                    break;
+                case 4:
+                    Console.Clear();
+                    Numeros.RestarNumeros(listaNumeros);
+                    Console.ReadKey();
+                    break;
+                case 5:
+                    Console.Clear();
+                    Numeros.MultiplicarNumeros(listaNumeros);
+                    Console.ReadKey();
+                    break;
+                case 6:
+                    Console.Clear();
+                    Numeros.DividirNumeros(listaNumeros);
+                    Console.ReadKey();
+                    break;
+                case 7:
+                    Console.Clear();
+                    Numeros.MostrarOperaciones(listaNumeros);
+                    Console.ReadKey();
+                    break;
+                case 8:
+                    menu = false;
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Presione cualquier tecla para continuar...");
+                    Console.ReadKey();
+                    break;
             }
-        case 2:
-            {
-                Console.Clear() ;
-                Numeros.MostrarNumeros(listaNumeros);
-                Console.ReadKey();
-                break;
-            }
-        case 3:
-            {
-                Console.Clear();
-                Numeros.MultiplicarNumeros(listaNumeros);
-                Console.ReadKey();
-                break;
-            }
+        }
     }
 }
